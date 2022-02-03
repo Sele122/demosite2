0ecb7ea72bd0e6fcb9449defdf05de5d4b1c3a62
Sele122 <93948938+Sele122@users.noreply.github.com>
2022-02-03 11:39:29 -0800

    Add files via upload

---
+++ demosite/WFS Projet/script11.js
@@ -0,0 +1,9 @@
+(function  () {
+    "use strict";
+    document.addEventListener('DOMContentLoaded', function(){
+var c = document.getElementById('current-time');
+c.innerHTML ='1:00PM'
+
+    }) ;
+
+})()
